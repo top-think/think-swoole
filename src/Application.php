a9b58ce607e58526cf491cd105aee5829ea0c359
My Name <my-name@chromium.org>
2018-10-26 18:58:17 +0800

    新增队列和Websocket支持

--- src/Application.php
+++ src/Application.php
@@ -25,8 +25,8 @@ class Application extends App
     /**
      * 处理Swoole请求
      * @access public
-     * @param  \Swoole\Http\Request    $request
-     * @param  \Swoole\Http\Response   $response
+     * @param  \Swoole\Http\Request $request
+     * @param  \Swoole\Http\Response $response
      * @param  void
      */
     public function swoole(Request $request, Response $response)
@@ -100,6 +100,60 @@ class Application extends App
         }
     }
 
+    public function swooleWebSocket($server, $frame)
+    {
+        try {
+            // 重置应用的开始时间和内存占用
+            $this->beginTime = microtime(true);
+            $this->beginMem  = memory_get_usage();
+
+            // 销毁当前请求对象实例
+            $this->delete('think\Request');
+            WebSocketFrame::destroy();
+            $request = $frame->data;
+            $request = json_decode($request, true);
+            // 重置应用的开始时间和内存占用
+            $this->beginTime = microtime(true);
+            $this->beginMem  = memory_get_usage();
+            WebSocketFrame::getInstance($server, $frame);
+            $_COOKIE                    = isset($request['arguments']['cookie']) ? $request['arguments']['cookie'] : [];
+            $_GET                       = isset($request['arguments']['get']) ? $request['arguments']['get'] : [];
+            $_POST                      = isset($request['arguments']['post']) ? $request['arguments']['post'] : [];
+            $_FILES                     = isset($request['arguments']['files']) ? $request['arguments']['files'] : [];
+            $_SERVER["PATH_INFO"]       = $request['url'] ?: '/';
+            $_SERVER["REQUEST_URI"]     = $request['url'] ?: '/';
+            $_SERVER["SERVER_PROTOCOL"] = 'http';
+            $_SERVER["REQUEST_METHOD"]  = 'post';
+
+
+            // 重新实例化请求对象 处理swoole请求数据
+            $this->request
+                ->withServer($_SERVER)
+                ->withGet($_GET)
+                ->withPost($_POST)
+                ->withCookie($_COOKIE)
+                ->withInput($request->rawContent())
+                ->withFiles($_FILES)
+                ->setBaseUrl($request->server['request_uri'])
+                ->setUrl($request->server['request_uri'] . (!empty($request->server['query_string']) ? '&' . $request->server['query_string'] : ''))
+                ->setHost($request->header['host'])
+                ->setPathinfo(ltrim($request->server['path_info'], '/'));
+
+            // 更新请求对象实例
+            $this->route->setRequest($this->request);
+
+            $resp = $this->run();
+            $resp->send();
+
+        } catch (HttpException $e) {
+            $this->webSocketException($server, $frame, $e);
+        } catch (\Exception $e) {
+            $this->webSocketException($server, $frame, $e);
+        } catch (\Throwable $e) {
+            $this->webSocketException($server, $frame, $e);
+        }
+    }
+
     protected function exception($response, $e)
     {
         if ($e instanceof \Exception) {
@@ -116,6 +170,17 @@ class Application extends App
             $response->status(500);
             $response->end($e->getMessage());
         }
+
+        throw $e;
+    }
+
+    protected function webSocketException($server, $frame, $e)
+    {
+        $response = [
+            'code'    => $e->code,
+            'content' => $e->getMessage(),
+        ];
+        $server->push($frame->fd, json_encode($response));
 
         throw $e;
     }
