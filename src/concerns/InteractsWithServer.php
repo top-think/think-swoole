2504275e217ddc019c7c10465a5c7250f14ad8b7
yunwuxin <448901948@qq.com>
2019-10-30 13:57:40 +0800

    workerStart事件增加app参数

--- src/concerns/InteractsWithServer.php
+++ src/concerns/InteractsWithServer.php
@@ -57,7 +57,7 @@ trait InteractsWithServer
 
         $this->bindSwooleTable();
 
-        $this->triggerEvent("workerStart");
+        $this->triggerEvent("workerStart", $this->app);
     }
 
     /**
