e931d7282f644e8183a83e18f940a0a3a05c55eb
yunwuxin <448901948@qq.com>
2020-07-10 01:18:22 +0800

    完善Context

--- src/coroutine/Context.php
+++ src/coroutine/Context.php
@@ -2,22 +2,69 @@
 
 namespace think\swoole\coroutine;
 
+use ArrayObject;
 use Closure;
 use Swoole\Coroutine;
 
 class Context
 {
 
+    /**
+     * 获取协程上下文
+     * @param null $cid
+     * @return Coroutine\Context
+     */
+    public static function get($cid = null)
+    {
+        return Coroutine::getContext($cid);
+    }
+
+    public static function getDataObject()
+    {
+        $context = self::get();
+        if (!isset($context['data'])) {
+            $context['data'] = new ArrayObject();
+        }
+        return $context['data'];
+    }
+
+    /**
+     * 获取当前协程临时数据
+     * @param string $key
+     * @param null $default
+     * @return mixed|null
+     */
     public static function getData(string $key, $default = null)
     {
-        return Coroutine::getContext()['data'][$key] ?? $default;
+        return self::getDataObject()[$key] ?? $default;
     }
 
+    /**
+     * 判断是否存在临时数据
+     * @param string $key
+     * @return bool
+     */
     public static function hasData(string $key)
     {
-        return isset(Coroutine::getContext()['data']) && array_key_exists($key, Coroutine::getContext()['data']);
+        return array_key_exists($key, self::getDataObject());
     }
 
+    /**
+     * 写入临时数据
+     * @param string $key
+     * @param $value
+     */
+    public static function setData(string $key, $value)
+    {
+        self::getDataObject()[$key] = $value;
+    }
+
+    /**
+     * 如果不存在则写入数据
+     * @param string $key
+     * @param $value
+     * @return mixed|null
+     */
     public static function rememberData(string $key, $value)
     {
         if (self::hasData($key)) {
@@ -34,28 +81,28 @@ class Context
         return $value;
     }
 
-    public static function setData(string $key, $value)
-    {
-        Coroutine::getContext()['data'][$key] = $value;
-    }
-
+    /**
+     * 删除数据
+     * @param string $key
+     */
     public static function removeData(string $key)
     {
-        unset(Coroutine::getContext()['data'][$key]);
+        unset(self::getDataObject()[$key]);
     }
 
-    public static function getDataKeys()
-    {
-        return array_keys(Coroutine::getContext()['data'] ?? []);
-    }
-
+    /**
+     * @internal
+     * 清空数据
+     */
     public static function clear()
     {
-        if (isset(Coroutine::getContext()['data'])) {
-            unset(Coroutine::getContext()['data']);
-        }
+        self::getDataObject()->exchangeArray([]);
     }
 
+    /**
+     * 获取当前协程ID
+     * @return mixed
+     */
     public static function getCoroutineId()
     {
         return Coroutine::getuid();
