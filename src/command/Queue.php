3d752bf75094be303c5a9182c5b54bc2743d5975
yunwuxin <448901948@qq.com>
2021-02-05 01:53:38 +0800

    完善

--- src/command/Queue.php
+++ src/command/Queue.php
@@ -15,6 +15,8 @@ class Queue extends Command
 
     public function handle(Manager $manager)
     {
+        $this->output->writeln('Swoole queue worker started');
+        $this->output->writeln('You can exit with <info>`CTRL-C`</info>');
         $manager->run();
     }
 }
