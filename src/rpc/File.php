df78b1f6eb6cd8f45f49ab7b0d4cc65595181504
yunwuxin <448901948@qq.com>
2021-04-29 18:48:04 +0800

    还原rpc\File

--- src/rpc/File.php
+++ src/rpc/File.php
@@ -6,27 +6,15 @@ use Throwable;
 
 class File extends \think\File
 {
-    protected $persistent = false;
-
-    /**
-     * 持久化该文件
-     */
-    public function persists()
-    {
-        $this->persistent = true;
-    }
-
     public function __destruct()
     {
         //销毁时删除临时文件
-        if ($this->persistent) {
-            try {
-                if (file_exists($this->getPathname())) {
-                    unlink($this->getPathname());
-                }
-            } catch (Throwable $e) {
-
+        try {
+            if (file_exists($this->getPathname())) {
+                unlink($this->getPathname());
             }
+        } catch (Throwable $e) {
+
         }
     }
 }
