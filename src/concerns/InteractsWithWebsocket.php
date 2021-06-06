12225b1344738349f6e1d8ee59abcb26a20f629c
yunwuxin <yunwuxin@users.noreply.github.com>
2021-06-06 04:27:58 +0000

    Apply fixes from StyleCI

--- src/concerns/InteractsWithWebsocket.php
+++ src/concerns/InteractsWithWebsocket.php
@@ -84,7 +84,7 @@ trait InteractsWithWebsocket
                         $frame->finish = false;
                     }
 
-                    $frame->data   .= $recv->data;
+                    $frame->data .= $recv->data;
                     $frame->finish = $recv->finish;
 
                     if ($frame->finish) {
