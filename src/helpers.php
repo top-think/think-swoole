8276c6193e89a7231948241ca147bf576e130b9a
yunwuxin <448901948@qq.com>
2021-11-18 18:45:08 +0800

    助手函数增加命名空间

--- src/helpers.php
+++ src/helpers.php
@@ -1,27 +1,30 @@
 <?php
 
-use think\swoole\response\File;
+namespace {
 
-if (!function_exists('swoole_cpu_num')) {
-    function swoole_cpu_num(): int
-    {
-        return 1;
+    if (!function_exists('swoole_cpu_num')) {
+        function swoole_cpu_num(): int
+        {
+            return 1;
+        }
+    }
+
+    if (!defined('SWOOLE_SOCK_TCP')) {
+        define('SWOOLE_SOCK_TCP', 1);
+    }
+
+    if (!defined('SWOOLE_PROCESS')) {
+        define('SWOOLE_PROCESS', 3);
+    }
+
+    if (!defined('SWOOLE_HOOK_ALL')) {
+        define('SWOOLE_HOOK_ALL', 1879048191);
     }
 }
 
-if (!defined('SWOOLE_SOCK_TCP')) {
-    define('SWOOLE_SOCK_TCP', 1);
-}
+namespace think\swoole\helper {
 
-if (!defined('SWOOLE_PROCESS')) {
-    define('SWOOLE_PROCESS', 3);
-}
-
-if (!defined('SWOOLE_HOOK_ALL')) {
-    define('SWOOLE_HOOK_ALL', 1879048191);
-}
-
-if (!function_exists('download')) {
+    use think\swoole\response\File;
 
     function download(string $filename, string $name = '', $disposition = File::DISPOSITION_ATTACHMENT): File
     {
@@ -33,9 +36,7 @@ if (!function_exists('download')) {
 
         return $response;
     }
-}
 
-if (!function_exists('file')) {
     function file(string $filename)
     {
         return new File($filename);
